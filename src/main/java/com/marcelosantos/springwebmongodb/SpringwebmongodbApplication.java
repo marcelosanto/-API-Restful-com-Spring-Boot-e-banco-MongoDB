10a4b490fa99f252e8936d5761121e08c9ea56db
Marcelo Santos <rocha.mer21@gmail.com>
2021-08-08 13:48:26 -0300

    User, UserResource

---
+++ src/main/java/com/marcelosantos/springwebmongodb/SpringwebmongodbApplication.java
@@ -0,0 +1,13 @@
+package com.marcelosantos.springwebmongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringwebmongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringwebmongodbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marcelosantos/springwebmongodb/SpringwebmongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcelosantos.springwebmongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringwebmongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
