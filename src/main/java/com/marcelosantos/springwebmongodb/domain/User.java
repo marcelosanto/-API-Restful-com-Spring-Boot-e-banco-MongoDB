303608d232e9600e25970bf01e2cb011b2168f4d
Marcelo Santos <rocha.mer21@gmail.com>
2021-08-12 23:24:44 -0300

    Referenciando os posts do usuário

--- src/main/java/com/marcelosantos/springwebmongodb/domain/User.java
+++ src/main/java/com/marcelosantos/springwebmongodb/domain/User.java
@@ -1,9 +1,12 @@
 package com.marcelosantos.springwebmongodb.domain;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Document(collection = "user")
@@ -15,7 +18,11 @@ public class User implements Serializable {
     private String name;
     private String email;
 
-    public User(){}
+    @DBRef(lazy = true) //Garante que os Posts so sao carregados quando for acessados
+    private List<Post> posts = new ArrayList<>();
+
+    public User() {
+    }
 
     public User(String id, String name, String email) {
         this.id = id;
@@ -58,5 +65,13 @@ public class User implements Serializable {
     @Override
     public int hashCode() {
         return Objects.hash(id);
+    }
+
+    public List<Post> getPosts() {
+        return posts;
+    }
+
+    public void setPosts(List<Post> posts) {
+        this.posts = posts;
     }
 }
