43e0560e3c593b1070c1fbe90a7d7f6b5d12ba02
Marcelo Santos <rocha.mer21@gmail.com>
2021-08-13 07:59:53 -0300

    Consulta com vários critérios

--- src/main/java/com/marcelosantos/springwebmongodb/resources/util/URL.java
+++ src/main/java/com/marcelosantos/springwebmongodb/resources/util/URL.java
@@ -2,8 +2,22 @@ package com.marcelosantos.springwebmongodb.resources.util;
 
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
 
 public class URL {
+
+    public static Date convertDate(String textDate, Date defaultValue) {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+        try {
+            return sdf.parse(textDate);
+        } catch (ParseException e) {
+            return defaultValue;
+        }
+    }
 
     public static String decodeParam(String text) {
         try {
