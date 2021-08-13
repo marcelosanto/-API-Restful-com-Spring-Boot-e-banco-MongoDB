3b135e0c6d88a253e8de21cf20abc953b1b65d1a
Marcelo Santos <rocha.mer21@gmail.com>
2021-08-12 23:35:41 -0300

    Obtendo um post por id

---
+++ src/main/java/com/marcelosantos/springwebmongodb/services/PostService.java
@@ -0,0 +1,23 @@
+package com.marcelosantos.springwebmongodb.services;
+
+import com.marcelosantos.springwebmongodb.domain.Post;
+import com.marcelosantos.springwebmongodb.repository.PostRepository;
+import com.marcelosantos.springwebmongodb.services.exception.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class PostService {
+
+    @Autowired
+    private PostRepository repo;
+
+    public Post findById(String id) {
+        Optional<Post> obj = repo.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+
+
+}
